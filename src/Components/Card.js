5aae5821da836b7ed76f0f97c82c75a0412e7fa3
Tahmid <tahmidmahin1729@gmail.com>
2023-03-27 19:52:42 +0600

    Halka CSS

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import mvc from '../Assets/multivariable_calculus.png'
 const Card = () => {
   return (
     <div className="course-card">
-        <img src = {mvc} alt="course" style={{height:'60%', width:'90%'}}/>
+        <img classname="course-image" src = {mvc} alt="course" style={{height:'60%', width:'100%'}}/>
         <div className="course-info">
             <h4><b>Course Name</b></h4>
             <p>Course Description</p>
